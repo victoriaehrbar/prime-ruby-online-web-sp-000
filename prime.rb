cf6b379fadc7b9982559e8bce5246ad87f0ddf57
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-02 02:55:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(number)
     return false
   else
     (2..number-1).to_a.all? do |possible_factor|
-      num % possible_factor != 0
+      number % possible_factor != 0
     end
   end
 end
