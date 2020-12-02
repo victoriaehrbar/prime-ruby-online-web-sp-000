d125a9bd9206ef42ada40be846eb07cc5ac1901d
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-02 02:55:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 def prime?(number)
-  if number < 0 or num == 0 or num == 1
+  if number < 0 or number == 0 or number == 1
     return false
   else
     (2..number-1).to_a.all? do |possible_factor|
